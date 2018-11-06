99b80ec34322bcf0f0ee652b1e8758502770dc32
Maximilian Kamp <maxkamp3@googlemail.com>
2018-11-06 13:59:26 +0100

    labeling from nodes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Graph2dComponent } from './pages/graph2d/graph2d.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { ConfigurationComponent } from './pages/configuration/configuration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Graph2dComponent
+    Graph2dComponent,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+      MatButtonModule,
+      MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/graph2d/graph2d.component.ts
+++ src/app/pages/graph2d/graph2d.component.ts
@@ -4,15 +4,16 @@ import {select} from 'd3';
 
 
 const TEST_DATA: any = {
-    'attributes': [{'x': 0, 'y': 5, 'name': 'A'}, {'x': 5, 'y': 0, 'name': 'B'}, {
-        'x': 0,
-        'y': -5,
-        'name': 'C'
-    }, {'x': -5, 'y': 0, 'name': 'D'}, {'x': 4, 'y': 4, 'name': 'E'}, {'x': -4, 'y': 4, 'name': 'F'}, {
-        'x': 4,
-        'y': -4,
-        'name': 'G'
-    }, {'x': -4, 'y': -4, 'name': 'H'}], 'fitness': 0.0, 'survivalProb': 0.0
+    'attributes': [{'x': 0, 'y': 5, 'name': 'A'},
+        {'x': 4, 'y': 4, 'name': 'E'},
+        {'x': 5, 'y': 0, 'name': 'B'},
+        {'x': 4, 'y': -4, 'name': 'G'},
+        {'x': 0, 'y': -5, 'name': 'C'},
+        {'x': -4, 'y': -4, 'name': 'H'},
+        {'x': -5, 'y': 0, 'name': 'D'},
+        {'x': -4, 'y': 4, 'name': 'F'}
+        ],
+    'fitness': 0.0, 'survivalProb': 0.0
 };
 
 export interface Point {
@@ -39,7 +40,6 @@ export class Graph2dComponent implements OnInit {
 
     constructor(private socketService: SocketService) {
         this.socketService.getStart().subscribe((data) => {
-            console.log('DATA', data); // TODO:
             //this.convertToGraph(data);
         });
     }
@@ -47,30 +47,10 @@ export class Graph2dComponent implements OnInit {
     ngOnInit() {
         this.svg = select('#graph')
             .append('svg')
-            .attr('width', '100%').attr('height', '100vh')
+            .attr('width', 600).attr('height', '80vh')
             .attr('border', '1px solid black');
 
         this.convertToGraph(TEST_DATA);
-        /*var nodes = [
-            {x: 10, y: 50},
-            {x: 70, y: 10},
-            {x: 140, y: 50},
-            {x: 70, y: 100}
-        ];
-
-        this.vis.selectAll('circle.nodes')
-            .data(nodes)
-            .enter()
-            .append('svg:circle')
-            .attr('cx', function (d) {
-                return d.x;
-            })
-            .attr('cy', function (d) {
-                return d.y;
-            })
-            .attr('r', '10px')
-            .attr('fill', 'black');*/
-
     }
 
     private convertToGraph(chromosome: Chromosome) {
@@ -78,8 +58,8 @@ export class Graph2dComponent implements OnInit {
 
         console.log(chromosome);
         for (const point of chromosome.attributes) {
-            point.x = (point.x + 6 ) * 50;
-            point.y = (point.y + 6 ) * 50;
+            point.x = (point.x + 6) * 50;
+            point.y = (point.y + 6) * 50;
             nodes.push(point);
         }
 
@@ -87,7 +67,7 @@ export class Graph2dComponent implements OnInit {
         for (let i = 0; i < nodes.length; i++) {
             const point = nodes[i];
             let point2;
-            if (i === nodes.length -1) {
+            if (i === nodes.length - 1) {
                 point2 = nodes[0];
             } else {
                 point2 = nodes[i + 1];
@@ -95,10 +75,10 @@ export class Graph2dComponent implements OnInit {
             linkedNodes.push({source: point, target: point2});
         }
 
-        this.svg.selectAll('circle.nodes')
+        const circles = this.svg.selectAll('circle')
             .data(nodes)
             .enter()
-            .append('svg:circle')
+            .append('circle')
             .attr('cx', (d) => {
                 return d.x;
             })
@@ -108,16 +88,39 @@ export class Graph2dComponent implements OnInit {
             .attr('r', '0.5em')
             .attr('fill', 'black');
 
-        this.svg.selectAll('.line')
+        const lines = this.svg.selectAll('.line')
             .data(linkedNodes)
             .enter()
             .append('line')
-            .attr('x1', function(d) { return d.source.x; })
-            .attr('y1', function(d) { return d.source.y; })
-            .attr('x2', function(d) { return d.target.x; })
-            .attr('y2', function(d) { return d.target.y; })
+            .attr('x1', function (d) {
+                return d.source.x;
+            })
+            .attr('y1', function (d) {
+                return d.source.y;
+            })
+            .attr('x2', function (d) {
+                return d.target.x;
+            })
+            .attr('y2', function (d) {
+                return d.target.y;
+            })
             .style('stroke', 'rgb(6,120,155)');
+
+        const text = this.svg.selectAll('text')
+            .data(nodes)
+            .enter()
+            .append('text')
+            .attr('x', (d) => {
+                return d.x;
+            })
+            .attr('y', (d) => {
+                return d.y + 30;
+            })
+            .text((d) => {
+                return d.name;
+            })
+            .attr('font-family', 'sans-serif')
+            .attr('font-size', '20px')
+            .attr('fill', 'red');
     }
-
-
 }
